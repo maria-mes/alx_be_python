d64faef0204503af8cda9d79afa17773b2094a9f
maria-mes <maramessay655@gmail.com>
2025-11-06 11:41:31 +0300

    Add Python introduction tasks for ALX

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1= 10
+number2= 5
+sum = number1 + number2
+difference = number1 - number2
+product = number1 * number2
+print("addition of 10 and 5 is:" , sum )
+print("subtraction of 10 and 5 is:" , difference)
+print("multiplication of 10 and 5 is:" , product)   
---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,4 @@
+age = int(input("How old are you? ").strip())
+years_to_add = 2050-2023
+future_age = age + years_to_add
+print(f"In the year 2050, you will be {future_age} years old.") 

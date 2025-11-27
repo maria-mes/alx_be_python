93eda3624afb7fb63faf4e23668a110c8f1298e3
maria-mes <maramessay655@gmail.com>
2025-11-27 18:31:46 +0300

    Reorganize main function in arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,16 +1,3 @@
-
-
-def main():
-    print("Arithmetic Operations")
-    num1 = float(input("Enter the first number: "))
-    num2 = float(input("Enter the second number: "))
-    operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
-
-    result = perform_operation(num1, num2, operation)
-    print(f"Result: {result}")
-
-
-    
 def perform_operation(num1, num2, operation):
     if operation == "add":
         return num1 + num2
@@ -24,5 +11,15 @@ def perform_operation(num1, num2, operation):
         return num1 / num2
     else:
         return "Error: Invalid operation"
+
+def main():
+    print("Arithmetic Operations")
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+    operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
+
+    result = perform_operation(num1, num2, operation)
+    print(f"Result: {result}")
+
 if __name__ == "__main__":
     main()

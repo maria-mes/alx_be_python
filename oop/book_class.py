e598433b78e964df0aa903bdc04f70022ae31a6b
maria-mes <maramessay655@gmail.com>
2025-12-11 09:53:25 +0300

    Implement main function for Book class demonstration
    Added main function to demonstrate Book class methods.

--- oop/book_class.py
+++ oop/book_class.py
@@ -1,5 +1,5 @@
 # book_class.py
-
+from book_class import Book
 class Book:
     def __init__(self, title: str, author: str, year: int):
         self.title = title
@@ -14,3 +14,19 @@ class Book:
 
     def __repr__(self):
         return f"Book('{self.title}', '{self.author}', {self.year})"
+
+def main():
+    # Creating an instance of Book
+    my_book = Book("1984", "George Orwell", 1949)
+
+    # Demonstrating the __str__ method
+    print(my_book)  # Expected to use __str__
+
+    # Demonstrating the __repr__ method
+    print(repr(my_book))  # Expected to use __repr__
+
+    # Deleting a book instance to trigger __del__
+    del my_book
+
+if __name__ == "__main__":
+    main()
